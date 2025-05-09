8a047e3e0d8e91270aabfa09bc5c11d6515ba1fc
shrouk-tarek <125499064+shrouk-tarek@users.noreply.github.com>
2025-05-09 02:22:50 +0200

    Update subjectController.js

--- controllers/subjectController.js
+++ controllers/subjectController.js
@@ -1,6 +1,8 @@
 const Subject = require('../models/Subject');
 const ErrorResponse = require('../utils/errorHandler');
 const Chapter = require('../models/Chapter');
+const Question = require('../models/Question');
+const StudentAnswer = require('../models/StudentAnswer');
 
 // @desc    Get all subjects
 // @route   GET /api/subjects
@@ -54,21 +56,36 @@ exports.addChapter = async (req, res, next) => {
   }
 };
 
-// @desc    Delete chapter from a subject
-// @route   DELETE /api/subjects/:subjectId/chapters/:chapterId
+// @desc    Delete a subject
+// @route   DELETE /api/subjects/:subjectId
 // @access  Private/Admin
-exports.deleteChapter = async (req, res, next) => {
+exports.deleteSubject = async (req, res, next) => {
   try {
-    const { chapterId } = req.params;
+    const { subjectId } = req.params;
 
-    const chapter = await Chapter.findByIdAndDelete(chapterId);
+    const subject = await Subject.findById(subjectId);
 
-    if (!chapter) {
+    if (!subject) {
       return res.status(404).json({
         success: false,
-        error: 'Chapter not found',
+        error: 'Subject not found',
       });
     }
+
+    // Delete related chapters
+    const chapters = await Chapter.find({ subjectId });
+    for (const chapter of chapters) {
+      await Question.deleteMany({ chapter: chapter.name, subjectId });
+      await StudentAnswer.deleteMany({ questionId: { $in: await Question.find({ chapter: chapter.name, subjectId }).select('_id') } });
+    }
+    await Chapter.deleteMany({ subjectId });
+
+    // Delete related questions and answers
+    await Question.deleteMany({ subjectId });
+    await StudentAnswer.deleteMany({ questionId: { $in: await Question.find({ subjectId }).select('_id') } });
+
+    // Delete the subject
+    await subject.deleteOne();
 
     res.status(200).json({
       success: true,
@@ -79,21 +96,28 @@ exports.deleteChapter = async (req, res, next) => {
   }
 };
 
-// @desc    Delete a subject
-// @route   DELETE /api/subjects/:subjectId
+// @desc    Delete chapter from a subject
+// @route   DELETE /api/subjects/:subjectId/chapters/:chapterId
 // @access  Private/Admin
-exports.deleteSubject = async (req, res, next) => {
+exports.deleteChapter = async (req, res, next) => {
   try {
-    const { subjectId } = req.params;
+    const { chapterId } = req.params;
 
-    const subject = await Subject.findByIdAndDelete(subjectId);
+    const chapter = await Chapter.findById(chapterId);
 
-    if (!subject) {
+    if (!chapter) {
       return res.status(404).json({
         success: false,
-        error: 'Subject not found',
+        error: 'Chapter not found',
       });
     }
+
+    // Delete related questions and answers
+    await Question.deleteMany({ chapter: chapter.name, subjectId: chapter.subjectId });
+    await StudentAnswer.deleteMany({ questionId: { $in: await Question.find({ chapter: chapter.name, subjectId: chapter.subjectId }).select('_id') } });
+
+    // Delete the chapter
+    await chapter.deleteOne();
 
     res.status(200).json({
       success: true,
